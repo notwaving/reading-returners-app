38e31b4000bd3b0deb0bd1c4daff1ecdff57a039
Samantha Burke <samanthaburke.uk@gmail.com>
2020-04-08 12:49:55 +0100

    refactors class based header component to stateless functional component

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-class Header extends React.Component {
-
-  render() {
-    return <div className="container-fluid padding">
-    <div className="header">
-      <div className="col-12">
-        <h1 className="display-5"><i className="fas fa-book-open"></i> Reading Returners</h1>
+function Header() {
+  return (
+    <div className="container-fluid padding">
+      <div className="header">
+        <div className="col-12">
+          <h1 className="display-5"><i className="fas fa-book-open"></i> Reading Returners</h1>
+        </div>
       </div>
     </div>
-  </div>
-  }
-
+  )
 }
+
 export default Header;
